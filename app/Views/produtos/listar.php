2a520c318570c054499f06c545904d4195e0e5b8
Igor Montuani <igor.montuani@outlook.com.br>
2023-06-27 20:28:21 -0300

    Alerta ao Cadastrar Produto

--- app/Views/produtos/listar.php
+++ app/Views/produtos/listar.php
@@ -74,6 +74,17 @@
                     </div>
                 </div>
             </div>
+            <?php if (isset($_GET['alert']) && $_GET['alert'] == "successCreate") : ?>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <h5><i class="icon fas fa-check"></i> Sucesso!</h5>
+                            Produto cadastrado com sucesso!
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
             <div class="row">
                 <div class="col-12">
                     <div class="card">
